017b4b6d1c6ffecdca3468fff71f75f2d2787824
NamNV <dev.namnv@gmail.com>
2022-11-30 23:10:05 +0700

    Fix count product deleted

--- src/controllers/statisticController.js
+++ src/controllers/statisticController.js
@@ -4,7 +4,8 @@ const StatisticController = {
   async dashboard(req, res) {
     try {
       const products = await ProductAmount.find().populate("prd_id").populate("size_id").populate("color").exec();
-      res.status(200).json({ totalProduct: products.filter((item) => item.amount).reduce((a, b) => a + b.amount, 0) })
+      const totalProduct = products.filter((item) => item.amount && item.prd_id).reduce((a, b) => a + b.amount, 0);
+      res.status(200).json({ totalProduct })
     } catch (e) {
       console.error(e);
       res.status(500)
