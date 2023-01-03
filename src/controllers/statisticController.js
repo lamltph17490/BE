9e775c0af58fd05950f0dc58d271b70d1b77020a
trinhvantudev <tutv1706@gmail.com>
2023-01-03 20:55:20 +0700

    Update dashboard

--- src/controllers/statisticController.js
+++ src/controllers/statisticController.js
@@ -15,12 +15,25 @@ const StatisticController = {
   async dashboard(req, res) {
     try {
       const yearSelected = +req.query.year || moment().year();
-      const startTime = moment(yearSelected, "YYYY").startOf("year").toDate();
-      const endTime = moment(yearSelected, "YYYY").endOf("year").toDate();
+      const monthSelected = +req.query.month || null;
+      const startOfYear = moment(yearSelected, "YYYY").startOf("year").toDate();
+      const endOfYear = moment(yearSelected, "YYYY").endOf("year").toDate();
+      let startTime = startOfYear;
+      let endTime = endOfYear;
+      const startOfDay = moment().startOf('day').toDate();
+      const endOfDay = moment().endOf('day').toDate();
+
+      if (monthSelected) {
+        startTime = moment(`${yearSelected}-${monthSelected}`, 'YYYY-MM').startOf('month').toDate();
+        endTime = moment(`${yearSelected}-${monthSelected}`, 'YYYY-MM').endOf('month').toDate();
+      }
+
       const products = await Product.find();
+      const ordersToday = await Order.find({ status: 4, date: { $gte: startOfDay, $lte: endOfDay } })
       const ordersCompleted = await Order.find({ status: 4, date: { $gte: startTime, $lte: endTime } });
+      const ordersInYear = await Order.find({ status: 4, date: { $gte: startOfYear, $lte: endOfYear } });
       const orderDetails = await OrderDetail.find({ orderId: { $in: ordersCompleted.map(({ _id }) => _id)}})
-      const orders = groupBy(ordersCompleted.map((i) => ({
+      const orders = groupBy(ordersInYear.map((i) => ({
         ...i.toObject(),
         month: moment(i.date).month() + 1,
       })), "month");
@@ -39,7 +52,6 @@ const StatisticController = {
       const totalOrders = ordersCompleted.length;
       const totalSaleProducts = getTotalSaleProducts(ordersCompleted);
       const totalRevenue = getTotalRevenue(ordersCompleted);
-      const ordersToday = ordersCompleted.filter(i => moment(i.createdAt) >= moment().startOf('day') && moment(i.createdAt) <= moment().endOf('day'))
       // const ordersToday = ordersCompleted
 
       res.status(200).json({ total: { totalOrders, totalProducts, totalRevenue, totalSaleProducts }, orders, populateProducts, ordersToday });
